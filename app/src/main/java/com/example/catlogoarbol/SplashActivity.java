7156885a54a5db071b0cab5c831997e87cc58880
Heriberthou <heriberto.gomezbolaina@aulavirtual.umar.mx>
2025-05-13 01:48:33 -0600

    carga
    pantalla de carga

---
+++ app/src/main/java/com/example/catlogoarbol/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.catlogoarbol;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Retraso de 2.5 segundos antes de pasar al inicio
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, DatosGeneralesActivity.class));
+            finish(); // Cierra Splash
+        }, 2500);
+    }
+}
